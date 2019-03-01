236a57609ee651d028f91369f2a9bca8e363f24b
Kasper Snæver Andersen <kasper@snaever.dk>
2019-03-01 17:53:36 +0100

    Using newly added modules

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VeeValidate from 'vee-validate'
+import {Tabs, Tab} from 'vue-tabs-component'
+import VueYouTubeEmbed from 'vue-youtube-embed'
+
+Vue.component('tabs', Tabs);
+Vue.component('tab', Tab);
 
 Vue.use(VeeValidate);
+Vue.use(VueYouTubeEmbed)
+
 Vue.config.productionTip = false
 
 new Vue({
