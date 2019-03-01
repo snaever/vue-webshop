0dfb62f27afaf3dd0ebcf0ea56b7ac9acd085b63
Kasper Snæver Andersen <kasper@snaever.dk>
2019-03-01 17:54:18 +0100

    Getting product information from json file

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,15 +2,25 @@
     <div class="product">
         <page-title bg="assets/img/brand-header.jpg">
             <h1 class="page-title__heading" slot="heading">Product <span class="page-title__heading--light">view</span></h1>
-            <p class="page-title__subheading" slot="subheading">Mens - Casuals - Hoodies &amp; Sweatshirts - Ave Classic Sweatshirt</p>
+            <p class="page-title__subheading" slot="subheading">{{ product.gender }} - {{ product.section }} - {{ product.category }} - {{ product.name }}</p>
         </page-title>
 
         <div class="content-container">
             <div class="product-info">
-                <div class="product-gallery"></div>
+                <div class="product-gallery">
+                    <hooper :itemsToShow="1">
+                        <slide v-for="(image, index) in product.images" :key="index">
+                            <picture class="product-gallery__image">
+                                <source :src="require('@/assets/img/items/' + image)">
+                                <img :src="require('@/assets/img/items/' + image)"/>
+                            </picture>
+                        </slide>
+                        <hooper-navigation slot="hooper-addons"></hooper-navigation>
+                    </hooper>
+                </div>
                 
                 <div class="product-data">
-                    <h2 class="product-data__title">Ave Classic Sweatshirt</h2>
+                    <h2 class="product-data__title">{{ product.name }}</h2>
 
                     <div class="product-meta">
                         <div class="product-reviews" title="70%">
@@ -47,44 +57,167 @@
 
                     </div>
 
-                    <p class="product-data__price">
-                        <span class="product-data__price--currency">£</span>
-                        <span class="product-data__price--regular">
-                            107
-                        </span>
-                        <span class="product-data__price--currency">£</span>89.99
-                    </p>
+                    <div class="product-data__price">
+                        <p v-if="product.sale.onSale">
+                            <span class="product-data__price--currency">£</span>
+                            <span class="product-data__price--regular">{{ product.sale.originalPrice }}</span>
+                        </p>
+                        <p>
+                            <span class="product-data__price--currency">£</span>{{ product.price }}
+                        </p>
+                    </div>
 
                     <div class="product-content">
-                        <p class="product-content__meta"><span class="product-content__meta--heading">Availability: </span>In stock</p>
-                        <p class="product-content__meta"><span class="product-content__meta--heading">Product code: </span>#499577</p>
-                        <p class="product-content__meta"><span class="product-content__meta--heading">Tags: </span><a href="#">Classic</a>, <a href="#">Casual</a>, <a href="#">V-neck</a>, <a href="#">Loose</a></p>
+                        <p class="product-content__meta"><span class="product-content__meta--heading">Availability: </span>
+                            <span v-if="product.stock > 10">
+                                In stock
+                            </span>
+                            <span v-else-if="product.stock == 0">
+                                Out of stock
+                            </span>
+                            <span v-else-if="product.stock < 10">
+                                Low stock
+                            </span>
+                        </p>
+                        <p class="product-content__meta"><span class="product-content__meta--heading">Product code: </span>#{{ product.sku }}</p>
+                        <p class="product-content__meta"><span class="product-content__meta--heading">Tags: </span>
+                            <span v-for="(tag, index) in product.tags" :key="index">
+                                <span v-if="index != 0">, </span><a href="#">{{ tag }}</a>
+                            </span>
+                        </p>
                     
                         <div class="product-content__description">
-                            <p>Donec sem lorem laoreet tempor un risus vitae, rutrum sodales nibh suspendisse est congue metus nunc, id viverra elit loreti rhoncus quis consecteur es. Donec pulvinar tempor lorem a pretium justo interdum.</p>
+                            {{ product.intro.body }}
                             <ul>
-                                <li>Elasticated cuffs</li>
-                                <li>Casual fit</li>
-                                <li>100% Cotton</li>
+                                <li v-for="(point, index) in product.intro.points" :key="index">{{ point }}</li>
                                 <li>Free shipping with 4 days delivery</li>
                             </ul>
                         </div>
+
+                        <form>
+                            <div class="product-options">
+                                <div class="product-options__select">
+                                    <label class="product-options__label" for="colour">Colour</label>
+                                    <div class="product-options__select-box">
+                                        <select id="colour" name="colour">
+                                            <option value="0" disabled selected>Select Colour</option>
+                                            <option v-for="(colour, index) in product.colours" :key="index">{{ colour }}</option>
+                                        </select>
+                                        <i class="fal fa-angle-down"></i>
+                                    </div>
+                                </div>
+
+                                <div class="product-options__select">
+                                    <label class="product-options__label" for="size">Size</label>
+                                    <div class="product-options__select-box">
+                                        <select id="size" name="size">
+                                            <option value="0" disabled selected>Select Size</option>
+                                            <option v-for="(size, index) in product.sizes" :key="index">{{ size }}</option>
+                                        </select>
+                                        <i class="fal fa-angle-down"></i>
+                                    </div>
+                                </div>
+
+                                <div class="product-options__qty">
+                                    <label class="product-options__label" for="qty">Qty</label>
+                                    <div class="quantity-toggle">
+                                        <input class="quantity-toggle__input" type="text" name="qty" id="qty" :value="quantity" readonly>
+                                        <div class="quantity-toggle__buttons">
+                                            <button type="button" @click="increment()"><i class="fal fa-angle-up"></i></button>
+                                            <button type="button" @click="decrement()"><i class="fal fa-angle-down"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="product-buttons">
+                                <button class="product-buttons__cta product-buttons__cta--submit" type="submit"><i class="far fa-shopping-cart"></i> Add to Cart</button>
+                                <button type="button" class="product-buttons__cta product-buttons__cta--lookbook"><i class="far fa-heart"></i> Add to Lookbook</button>
+                            </div>
+
+                            <button type="button" class="product-content__compare-btn"><i class="far fa-compress-alt"></i> Add to Compare</button>
+
+                        </form>
+
                     </div>
                     
                 </div>
 
             </div>
+            
+            <div class="product-details">
+                <tabs :options="{ useUrlFragment: false }">
+                    <tab name="Description">
+                        <p class="product-details__title">{{ product.description.heading }}</p>
+                        <p>{{ product.description.body }}</p>
+                    </tab>
+                    <tab name="Video">
+                        <div v-if="product.video.isVideo">
+                            <youtube :video-id="product.video.youtubeId"></youtube>
+                        </div>
+                    </tab>
+                    <tab name="Size &amp; specs">
+                        Doesn't take up much space in the closet
+                    </tab>
+                    <tab name="Delivery &amp; returns">
+                        Free shipping with 4 days delivery
+                    </tab>
+                    <tab name="Reviews">
+                        Very good
+                    </tab>
+                </tabs>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '@/components/PageTitle.vue'
+import axios from "axios";
+import {
+    Hooper,
+    Slide,
+    Navigation as HooperNavigation 
+} from 'hooper';
+import 'hooper/dist/hooper.css';
 
 export default {
-    name: 'app',
+    name: 'product',
     components: {
-        PageTitle
+        PageTitle,
+        Hooper,
+        Slide,
+        HooperNavigation
+    },
+    data () {
+        return {
+            product: {},
+            quantity: 1,
+        }
+    },
+    created() {
+        this.getData();
+    },
+    methods: {
+        getData() {
+            let productID = this.$route.params.id;
+
+            axios
+                .get("/data.json")
+                .then(response => (this.product = response.data[productID]))
+                .catch(err => console.log(err.message));
+        },
+        increment() {
+            this.quantity++
+        },
+        decrement() {
+            if(this.quantity === 1) {
+                /* It should not be possible to buy less than one item */
+            } else {
+                this.quantity--
+            }
+        }
     }
 }
 </script>
@@ -101,6 +234,43 @@ export default {
         height: 710px;
         margin-right: 15px;
         background-color: lightgray;
+
+        .hooper {
+            height: 100%;
+
+            &-slide {
+                width: 100%!important;
+            }
+
+            &-next, &-prev {
+                transition: 0.1s;
+
+                path:last-child {
+                    transition: 0.1s;
+                }
+
+                &:hover {
+                    &:not(.is-disabled) {
+                        background-color: #00c8c8;
+
+                        path:last-child {
+                            fill: white;
+                        }
+                    }
+                }
+            }
+        }
+
+        &__image {
+            width: 100%;
+            height: 100%;
+
+            img {
+                display: block;
+                width: 100%;
+                margin: 0;
+            }
+        }
     }
 
     .product-data {
@@ -109,15 +279,16 @@ export default {
 
         &__title {
             margin: 0;
-            font-weight: 400;
+            font-weight: 500;
             color: #575153;
             text-transform: uppercase;
             font-size: 1.4em;
         }
 
         &__price {
+            display: flex;
             font-size: 2.2em;
-            font-weight: 100;
+            font-weight: 300;
             color: #9a9a9a;
 
             &--regular {
@@ -137,7 +308,7 @@ export default {
         align-items: center;
         margin-top: 20px;
         color: #727272;
-        font-weight: 400;
+        font-weight: 500;
 
         &__text {
             margin: 0 10px;
@@ -204,12 +375,235 @@ export default {
             font-family: 'Roboto', sans-serif;
             font-size: 1.1em;
             line-height: 1.5em;
-            margin-top: 30px;
+            margin: 30px 0;
             color: #8e8c8c;
 
             ul {
                 padding: 0 20px;
             }
+        }
+
+        &__compare-btn {
+            border: 0;
+            padding: 0;
+            margin-top: 30px;
+            color: #737373;
+            text-transform: uppercase;
+            text-decoration: none;
+            font-size: 1.2em;
+            letter-spacing: 2px;
+            z-index: 3;
+            transition: 0.2s;
+            cursor: pointer;
+            transition: 0.1s;
+
+            .far {
+                background-color: #727272;
+                color: #ffffff;
+                border-radius: 100%;
+                width: 30px;
+                height: 30px;
+                line-height: 30px;
+                padding-left: 3px;
+                box-sizing: border-box;
+                transition: 0.1s;
+            }
+
+            &:hover {
+                color: #00c8c8;
+
+                .far {
+                    background-color: #00c8c8;
+                }
+            }
+        }
+    }
+
+    .product-options {
+        display: flex;
+        justify-content: space-between;
+        width: 500px;
+        margin-top: 50px;
+
+        &__select-box {
+            position: relative;
+            width: 170px;
+            border: 1px solid #e7e7e7;
+            overflow: hidden;
+            background: #ffffff;
+
+            select {
+                padding: 5px 8px;
+                height: 35px;
+                width: 130%;
+                color: #989898;
+                border: none;
+                box-shadow: none;
+                background: transparent;
+                background-image: none;
+                -webkit-appearance: none;
+                cursor: pointer;
+            }
+
+            .fa-angle-down {
+                position: absolute;
+                top: 11px;
+                right: 6px;
+                color: #989898;
+            }
+        }
+
+        &__label {
+            display: block;
+            font-weight: 700;
+            text-transform: uppercase;
+            color: #575153;
+            margin-bottom: 10px;
+        }
+    }
+
+    .quantity-toggle {
+        display: flex;
+
+        &__input {
+            height: 37px;
+            width: 35px;
+            text-align: center;
+            padding: 5px 8px;
+            box-sizing: border-box;
+            border: 1px solid #e7e7e7;
+            border-right: 0;
+            cursor: default;
+            color: #989898;
+        }
+
+        &__buttons {
+            button {
+                display: block;
+                border: 1px solid #e7e7e7;
+                color: #989898;
+                height: 18.5px;
+                width: 35px;
+                cursor: pointer;
+
+                &:first-child {
+                    border-bottom: 0;
+                }
+            }
+        }
+    }
+
+    .product-buttons {
+        margin-top: 50px;
+        width: 530px;
+        display: flex;
+        justify-content: space-between;
+
+        &__cta {
+            border: 2px solid #737373;
+            padding: 15px 30px;
+            color: #737373;
+            text-transform: uppercase;
+            text-decoration: none;
+            font-size: 1.2em;
+            letter-spacing: 2px;
+            z-index: 3;
+            transition: 0.2s;
+            cursor: pointer;
+
+            &--submit {
+                &:hover {
+                    border-color: #333333;
+                    background-color: #333333;
+                    color: #fff;
+                }
+            }
+
+            &--lookbook {
+                &:hover {
+                    border-color: #00c8c8;
+                    background-color: #00c8c8;
+                    color: #fff;
+                }
+            }
+        }
+    }
+
+    .product-details {
+        &__title {
+            font-family: 'Montserrat', sans-serif;
+            text-transform: uppercase;
+            color: #727272;
+            font-weight: 500;
+        }
+        
+        .tabs-component {
+            margin: 4em 0;
+        }
+
+        .tabs-component-tabs {
+            background-color: #f8f8f8;
+            margin: 0;
+        }
+
+        @media (min-width: 700px) {
+            .tabs-component-tabs {
+                padding: 0;
+                border: 0;
+                align-items: stretch;
+                display: flex;
+                justify-content: flex-start;
+            }
+        }
+
+        .tabs-component-tab {
+            text-transform: uppercase;
+            color: #575153;
+            height: 50px;
+            line-height: 50px;
+            font-size: 14px;
+            font-weight: 600;
+            margin-right: 0;
+            list-style: none;
+        }
+
+        .tabs-component-tab.is-active {
+            color: #fff;
+            background-color: #333333;
+        }
+
+        .tabs-component-tab.is-disabled * {
+            color: #cdcdcd;
+            cursor: not-allowed !important;
+        }
+
+        @media (min-width: 700px) {
+            .tabs-component-tab {
+                margin-right: 30px;
+                transition: transform .3s ease;
+            }
+
+            .tabs-component-tab.is-active {
+                border-bottom: solid 1px #fff;
+                z-index: 2;
+                transform: translateY(0);
+            }
+        }
+
+        .tabs-component-tab-a {
+            align-items: center;
+            color: inherit;
+            display: flex;
+            padding: 0 1em;
+            text-decoration: none;
+        }
+
+        .tabs-component-panels {
+            padding: 10px 0;
+            font-family: 'Roboto', sans-serif;
+            font-size: 1.1em;
+            line-height: 1.5em;
+            color: #8e8c8c;
         }
     }
 </style>
