cbceb00d568233a147db32139dd37af0cf53c41d
Kasper Snæver Andersen <kasper@snaever.dk>
2019-03-02 20:30:17 +0100

    Declaring service worker

--- src/main.js
+++ src/main.js
@@ -17,3 +17,9 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
