3e816cb1346a5d753f65aea4856d10ebd02b2052
Kasper Snæver Andersen <kasper@snaever.dk>
2019-03-02 17:41:06 +0100

    General style changes

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,11 @@
 @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700|Roboto');
 @import "@/assets/sass/_all.scss";
 
+  html, body {
+      max-width: 100%;
+      overflow-x: hidden;
+  }
+
   body {
     font-family: $main-font;
     font-size: 13px;
@@ -30,16 +35,24 @@
 
   .site-container {
     max-width: 1700px;
+    padding: 0 20px;
     margin: auto;
   }
 
   .content-container {
     max-width: 1170px;
+    padding: 0 20px;
     margin: auto;
   }
 
-  @media (min-width: $breakpoint-tablet) {
+  @media (max-width: $breakpoint-tablet) {
       .desktop {
+        display: none;
+      }
+  }
+
+    @media (min-width: $breakpoint-tablet + 1) {
+      .mobile {
         display: none;
       }
   }
